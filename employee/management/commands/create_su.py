29497b32f4711dbd60b2c29397ce59aacaed3faa
aleksandr <sanek_ves@mail.ru>
2023-10-07 18:04:09 +0300

    Кастомная команда создания суперпользователя

---
+++ employee/management/commands/create_su.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from employee.models import Employee
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        """Команда для создания суперпользователя"""
+        employee = Employee.objects.create(email='admin@admin.ru',
+                                       first_name='admin',
+                                       last_name='admin',
+                                       is_staff=True,
+                                       is_superuser=True)
+
+        employee.set_password('admin')
+        employee.save()
