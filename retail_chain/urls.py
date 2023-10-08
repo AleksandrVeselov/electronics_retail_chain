5a737c7b40a61ad6a51ffab6561ab4f0a50ce3a8
aleksandr <sanek_ves@mail.ru>
2023-10-08 19:19:19 +0300

    Настроено обнуление долга перед поставщиком

--- retail_chain/urls.py
+++ retail_chain/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from retail_chain.apps import RetailChainConfig
-from retail_chain.views import RetailChainView, RetailChainDetailView
+from retail_chain.views import RetailChainView, RetailChainDetailView, clear_debt
 
 app_name = RetailChainConfig.name
 
 urlpatterns = [
     path('', RetailChainView.as_view(), name='retail-chain-list'),
-    path('link/<int:pk>', RetailChainDetailView.as_view(), name='retail-chain')
+    path('link/<int:pk>', RetailChainDetailView.as_view(), name='retail-chain'),
+    path('clear-debt/<int:pk>', clear_debt, name='clear-debt'),
 ]
--- retail_chain/views.py
+++ retail_chain/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 
 from retail_chain.models import Link
@@ -15,3 +16,13 @@ class RetailChainDetailView(DetailView):
     """Контроллер для отображения страницы с информацией о звене сети"""
     model = Link
     template_name = 'retail_chain/link_detail.html'
+
+
+def clear_debt(request, pk):
+    """контроллер для обнуления задолженности перед поставщиком"""
+
+    link = Link.objects.get(pk=pk)  # получаем нужное звено сети
+    link.debt = 0  # обнуляем задолженность перед поставщиком
+    link.save()  # сохраняем результат
+
+    return redirect(reverse('retail-chain:retail-chain-list'))
